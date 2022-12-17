667ae9da109d14d76a361c4034f9e8b4c8ce16e3
prashantkaushik97 <kaushik.prashant00@gmail.com>
2022-12-17 04:02:25 -0500

    sse

---
+++ services/order.js
@@ -0,0 +1,79 @@
+const { nanoid } = require("nanoid");
+const PostModel = require("../models/postModel");
+
+const getOrdersService = async () => {
+//   try {
+//     const posts = await PostModel.find();
+//     const _length = posts.length;
+//     const message = _length === 0 ? "Not found" : "success";
+//     const error = _length === 0 ? true : false;
+//     const statusCode = _length === 0 ? 404 : 200;
+//     return { data: posts, error, message, statusCode };
+//   } catch (error) {
+//     return {
+//       data: [],
+//       error: true,
+//       message: "Sorry an error occurred",
+//       statusCode: 500,
+//     };
+//   }
+try {
+    const order = await Order.findOne(query);
+
+    res.status(201).json({ order });
+} catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Server Error" });
+}
+};
+
+const createPostService = async (post) => {
+  try {
+    const _post = await PostModel.create(post);
+    return { data: [_post], error: false, message: "success", statusCode: 200 };
+  } catch (error) {
+    return {
+      data: [],
+      error: true,
+      message: "Sorry an error occurred",
+      statusCode: 500,
+    };
+  }
+};
+
+const updatePostReaction = async (postId, userId) => {
+  try {
+    const post = await PostModel.findByIdAndUpdate(
+      postId,
+      { $push: { likers: userId } },
+      { new: true }
+    );
+    return { data: [post], error: false, message: "success", statusCode: 200 };
+  } catch (error) {
+    return {
+      data: [],
+      error: true,
+      message: "Sorry an error occurred",
+      statusCode: 500,
+    };
+  }
+};
+
+const createNotification = ({ postId, authorId, userId }) => {
+  return {
+    id: nanoid(),
+    userId: authorId,
+    title: `Your post has been liked by user ${userId}`,
+    type: "post",
+    meta: {
+      id: postId,
+    },
+  };
+};
+
+module.exports = {
+  getPostsService,
+  createPostService,
+  updatePostReaction,
+  createNotification,
+};
